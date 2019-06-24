12f62ec64eb33759c1f5202b0c76dcb12aa9cb31
BradleyJrichardson <brdl.richardson@gmail.com>
2019-06-24 18:06:27 +1000

    Success! able to scrap the wiki page for the key information needed, works will all types of mushrooms

--- backend/server.js
+++ backend/server.js
@@ -46,48 +46,10 @@ app.get("/mushroom", (req, res) => {
       section1 = data.remaining.sections[0].text;
       section2 = data.remaining.sections[1].text;
       section3 = data.remaining.sections[2].text;
-      section4 = data.remaining.sections[3].text;
-      section5 = data.remaining.sections[4].text;
-      section6 = data.remaining.sections[5].text;
-      section7 = data.remaining.sections[6].text;
-      section8 = data.remaining.sections[7].text;
-      const blockToParse =
-        section0 +
-        section1 +
-        section2 +
-        section3 +
-        section4 +
-        section5 +
-        section6 +
-        section7 +
-        section8;
-
-      const cleanedMushroom = scrape(blockToParse);
-      const mycology = cleanedMushroom.mycology;
-      const startIndex = mycology.indexOf("Mycological characteristic");
-      const slicedMycology = mycology.slice(startIndex);
-      console.log(slicedMycology);
-
-      const indexOfEnd = (string, slicedMycology) => {
-        var io = slicedMycology.indexOf(string);
-        return io == -1 ? -1 : io + string.length;
-      };
-
-      const lastIndexOfHeader = indexOfEnd(
-        "Mycological characteristic",
-        slicedMycology
-      );
-      console.log(lastIndexOfHeader);
+      const blockToParse = section0 + section1 + section2 + section3;
+      const processedMushroom = scrape(blockToParse);
+      console.log(processedMushroom);
     })
-
-    /// Mycological characteristic
-    // spores on hymenium
-    // cap is convex
-    // hymenium is adnate or subdecurrent
-    // stipe has a ring
-    // spore print is brown
-    // ecology is mycorrhizal
-    // edibility: edible
 
     .catch(err => {
       console.log(err);
@@ -96,18 +58,3 @@ app.get("/mushroom", (req, res) => {
 
 // listen on port 3000
 app.listen(5000, () => console.log("listening on port 5000"));
-
-/// how to get second third or etc index of
-// var secondLastIndex = url.lastIndexOf('/', url.lastIndexOf('/')-1)
-// var thirdLastIndex = url.lastIndexOf('/', (url.lastIndexOf('/', url.lastIndexOf('/')-1) -1))
-// const string = blockToParse.replace(/<[^>]*>?/gm, "");
-
-function nthIndex(str, pat, n) {
-  var L = str.length,
-    i = -1;
-  while (n-- && i++ < L) {
-    i = str.indexOf(pat, i);
-    if (i < 0) break;
-  }
-  return i;
-}
--- backend/webscraper/webscraper.js
+++ backend/webscraper/webscraper.js
@@ -5,19 +5,92 @@ const scrape = dirtyData => {
   const $ = cheerio.load(dirtyData);
   // jsonframe($);
 
-  const tableData = $(".infobox")
+  const mycology = $(".infobox").text();
+
+  const startIndex = mycology.indexOf("Mycological characteristics");
+  const slicedMycology = mycology.slice(startIndex);
+
+  const indexOfEnd = (string, slicedMycology) => {
+    var io = slicedMycology.indexOf(string);
+    return io == -1 ? -1 : io + string.length;
+  };
+
+  const lastIndexOfHeader = indexOfEnd(
+    "Mycological characteristics",
+    slicedMycology
+  );
+
+  const LastIndexOf1 = indexOfEnd("hymenium", slicedMycology);
+
+  const hymeniumSporeType = slicedMycology
+    .slice(lastIndexOfHeader, LastIndexOf1)
+    .replace(/(\r\n|\n|\r)/gm, "")
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const firstIndexOf3 = slicedMycology.lastIndexOf("hymenium");
+  const capType = slicedMycology
+    .slice(LastIndexOf1, firstIndexOf3)
+    .replace(/(\r\n|\n|\r)/gm, "")
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const firstIndexOf4 = slicedMycology.indexOf("stipe");
+  const hymeniumShapeType = slicedMycology
+    .slice(firstIndexOf3, firstIndexOf4)
+    .replace(/(\r\n|\n|\r)/gm, "")
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const firstIndexOf5 = slicedMycology.lastIndexOf("spore");
+  const stipeType = slicedMycology
+    .slice(firstIndexOf4, firstIndexOf5)
+    .replace(/(\r\n|\n|\r)/gm, "")
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const firstIndexOf6 = slicedMycology.indexOf("ecology");
+  const ecologyType = slicedMycology
+    .slice(firstIndexOf5, firstIndexOf6)
+    .replace(/(\r\n|\n|\r)/gm, "")
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const firstIndexOf7 = slicedMycology.indexOf("edibility");
+  const edibilityType = slicedMycology
+    .slice(firstIndexOf7)
+    .replace(/(\r\n|\n|\r)/gm, "")
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const mycologyObj = {
+    hymeniumSporeType: hymeniumSporeType,
+    capType: capType,
+    hymeniumShapeType: hymeniumShapeType,
+    stipeType: stipeType,
+    ecologyType: ecologyType,
+    edibilityType: edibilityType
+  };
+
+  const doublestring = $(".infobox")
     .find("tbody")
     .find("tr")
-    .find("td")
+    .find("th")
+    .find("i")
     .text();
 
+  const firstChar = doublestring[0];
+  const indexToSplit = doublestring.lastIndexOf(firstChar);
+  const slicedName = doublestring.slice(indexToSplit);
+
   return {
+    bionomial: slicedName,
     kingdom: $(".kingdom").text(),
     division: $(".division").text(),
     class: $(".class").text(),
     order: $(".order").text(),
     family: $(".family").text(),
-    mycology: $(".infobox").text()
+    mycology: mycologyObj
   };
 };
 
